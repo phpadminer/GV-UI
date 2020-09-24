e07df65f3ef864d5966a586f98d1f0e8d107fdf6
ghosthao <wlt157459@163.com>
2020-09-24 19:53:22 +0800

    [新增] 1.增加button组件icon;2.新增控制icon位置和名称的props

--- src/button.vue
+++ src/button.vue
@@ -1,26 +1,82 @@
 <template>
-    <button class="gv-button">提交</button>
+    <button class="gv-button" :class="{'icon-default':true,[`icon-${iconPosition}`]:true}">
+        <svg class="icon" v-if="icon">
+            <use :xlink:href="`#gv-i-${icon}`"></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
 </template>
 <script>
-    export default {}
+    export default {
+        props: {
+            icon: {
+                type: String,
+            },
+            iconPosition: {
+                type: String,
+                default: 'left',
+                validator: function (value) {
+                    return ['left', 'right'].indexOf(value) > -1;
+                }
+            }
+        }
+    };
 </script>
 <style lang="scss">
-    .gv-button{
+    .icon {
+        width: 1em;
+        height: 1em;
+    }
+
+    .gv-button {
         font-size: var(--font-size);
         height: var(--button-height);
         padding: 0 1em;
         border-radius: var(--border-radius);
-        border:1px solid var(--border-color);
+        border: 1px solid var(--border-color);
         background: var(--button-bg);
-        &:hover{
-            border-color:var(--border-color-hover);
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+
+        &:hover {
+            border-color: var(--border-color-hover);
         }
-        &:active{
+
+        &:active {
             background-color: var(--button-active-bg);
         }
-        &:focus{
+
+        &:focus {
             outline: none;
         }
+
+        &.icon-default, &.icon-left {
+            > .content {
+                order: 2
+            }
+
+            > .icon {
+                order: 1;
+                margin-right: 0.3em;
+                margin-left: 0;
+            }
+        }
+
+        &.icon-right {
+            > .content {
+                order: 1
+            }
+
+            > .icon {
+                order: 2;
+                margin-right: 0;
+                margin-left: 0.3em;
+            }
+        }
+
     }
 
 </style>
