6f85ddd9a6dcc541409bdf742aa264bc05859c00
ghosthao <wlt157459@163.com>
2020-09-25 21:28:45 +0800

    [新增]1.修改karma配置文件 新增ChromeHeadless

--- karma.conf.js
+++ karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function (config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        browsers: ['Chrome','ChromeHeadless'],
+        browsers: ['ChromeHeadless'],
         singleRun: false,
         concurrency: Infinity
 
