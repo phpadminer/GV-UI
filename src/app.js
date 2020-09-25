2b0b7c956f1b5287be0c0c5bee119eedc0617ae9
ghosthao <wlt157459@163.com>
2020-09-25 13:53:20 +0800

    [新增] 1.新增group-button组件；2.新增对group-button子元素的监听

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import Vue from "vue";
 import Button from "./button";
 import Icon from "./icon";
+import GroupButton from "./group-button";
 Vue.component('gv-button', Button);
 Vue.component('gv-icon', Icon);
+Vue.component('gv-group-button', GroupButton);
 new Vue({
     el: "#app",
     data(){
---
+++ src/group-button.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="gv-group-button">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "group-button",
+        mounted() {
+            for(let node of this.$el.children){
+                let nodeName = node.nodeName.toLowerCase();
+                if(nodeName !== 'button'){
+                    console.warn(`gv-group-button的子元素需要的是button,你所给的是${nodeName}`);
+                }
+            }
+        }
+    };
+</script>
+
+<style scoped lang="scss">
+    .gv-group-button {
+        display: inline-flex;
+        vertical-align: middle;
+
+        > .gv-button {
+            border-radius: 0;
+            margin-left: -1px;
+
+            &:first-child {
+                border-top-left-radius: var(--border-radius);
+                border-bottom-left-radius: var(--border-radius);
+            }
+
+            &:last-child {
+                border-top-right-radius: var(--border-radius);
+                border-bottom-right-radius: var(--border-radius);
+            }
+
+            &:hover {
+                position: relative;
+                z-index: 1;
+            }
+        }
+    }
+</style>
