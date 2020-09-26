39d47519418430e7722be77783ff5ec9150e97db
ghosthao <wlt157459@163.com>
2020-09-26 22:09:42 +0800

    新增input组件

--- src/app.js
+++ src/app.js
@@ -2,12 +2,12 @@ import Vue from "vue";
 import Button from "./button";
 import Icon from "./icon";
 import GroupButton from "./group-button";
-import chai from 'chai';
-import spies from 'chai-spies';
+import Input from "./input";
 
 Vue.component('gv-button', Button);
 Vue.component('gv-icon', Icon);
 Vue.component('gv-group-button', GroupButton);
+Vue.component('gv-input', Input);
 new Vue({
     el: "#app",
     data() {
@@ -16,66 +16,4 @@ new Vue({
         };
     }
 });
-chai.use(spies);
-const expect = chai.expect;
-const spy = chai.spy(() => {
-});
-// 单元测试
-// 测试图标
-{
-    // const Constructor = Vue.extend(Button);
-    // const vm = new Constructor({
-    //     propsData: {
-    //         icon: "correct"
-    //     }
-    // });
-    // vm.$mount('#test');
-    // let useElement = vm.$el.querySelector('use');
-    // let href = useElement.getAttribute("xlink:href");
-    // expect(href).to.eq("#gv-i-correct");
-}
-// 测试loading
-// {
-//     const Constructor = Vue.extend(Button);
-//     const vm = new Constructor({
-//         propsData: {
-//             icon: "correct",
-//             loading:true
-//         }
-//     });
-//     vm.$mount('#test');
-//     let useElement = vm.$el.querySelector('use');
-//     let href = useElement.getAttribute("xlink:href");
-//     expect(href).to.eq("#gv-i-loading");
-// }
-// 测试iconPosition
-// {
-//     const Constructor = Vue.extend(Button);
-//     const vm = new Constructor({
-//         propsData: {
-//             icon: "correct",
-//             loading:true,
-//             iconPosition:"right"
-//         }
-//     });
-//     vm.$mount('#test');
-//     let svgElement = vm.$el.querySelector('svg');
-//     let {order} = window.getComputedStyle(svgElement);
-//     expect(order).to.eq("2");
-// }
-// 测试click
-{
-    const Constructor = Vue.extend(Button);
-    const vm = new Constructor({
-        propsData: {
-            icon: "correct",
-            loading: true,
-            iconPosition: "right"
-        }
-    });
-    vm.$mount();
-    vm.$on('click', spy);
-    const button = vm.$el;
-    button.click();
-    expect(spy).to.have.been.called();
-}
+
---
+++ src/input.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="gv-input">
+        <input :value="value" type="text"/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "gv-input",
+        props: {
+            value: {
+                type: String
+            }
+        }
+    };
+</script>
+
+<style lang="scss">
+    /*--button-height: 32px;*/
+    /*--font-size: 14px;*/
+    /*--button-bg: white;*/
+    /*--button-active-bg: #eee;*/
+    /*--border-radius: 4px;*/
+    /*--color: #999;*/
+    /*--border-color: #999;*/
+    /*--border-color-hover: #666;*/
+    $height: 32px;
+    $fontSize: 14px;
+    $borderRadius: 4px;
+    $borderColor: #999;
+    $borderColorHover:#666;
+
+    * {
+        box-sizing: border-box;
+    }
+
+    .gv-input {
+        font-size: $fontSize;
+        & > input {
+            height: $height;
+            font-size: inherit;
+            padding: 0 8px;
+            border-width: 1px;
+            border-color: $borderColor;
+            border-radius: $borderRadius;
+            &:hover{
+                border-color: $borderColorHover;
+            }
+            &:focus{
+                border-color:rgba(0,40,100,0.3)
+            }
+        }
+
+    }
+</style>
