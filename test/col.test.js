1abb137baad0b2c9e4bd9765bd1355d6e4a38bec
ghosthao <wlt157459@163.com>
2020-09-28 20:17:05 +0800

    [修改]1.格式化col.test.js

--- test/col.test.js
+++ test/col.test.js
@@ -10,32 +10,32 @@ describe('Col', () => {
     it('存在.', () => {
         expect(Col).to.be.ok;
     });
-    describe('测试属性',()=>{
-        it('设置span=2 会出现 col-span-2类名',()=>{
+    describe('测试属性', () => {
+        it('设置span=2 会出现 col-span-2类名', () => {
             let divElement = document.createElement('div');
             const Constructor = Vue.extend(Col);
             let vm = new Constructor({
-                propsData:{
-                    span:'2',
+                propsData: {
+                    span: '2',
                 }
-            })
+            });
             vm.$mount(divElement);
             expect(vm.$el.classList.contains('col-span-2')).to.be.true;
-            vm.$destroy()
-        })
-        it('设置offset=2 会出现 col-offset-2类名',()=>{
+            vm.$destroy();
+        });
+        it('设置offset=2 会出现 col-offset-2类名', () => {
             let divElement = document.createElement('div');
             const Constructor = Vue.extend(Col);
             let vm = new Constructor({
-                propsData:{
-                    offset:'2',
+                propsData: {
+                    offset: '2',
                 }
-            })
+            });
             vm.$mount(divElement);
             expect(vm.$el.classList.contains('col-offset-2')).to.be.true;
-            vm.$destroy()
-        })
-    })
+            vm.$destroy();
+        });
+    });
 
 
 });
